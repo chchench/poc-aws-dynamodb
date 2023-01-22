29b4052855b30be4f669aa8b79f1413531e5950f
Charles H. Cheng <45220401+chchench@users.noreply.github.com>
2023-01-22 12:00:41 -0800

    Cleaned up PoC code

--- go.mod
+++ go.mod
@@ -11,10 +11,13 @@ require (
 require (
 	github.com/aws/aws-sdk-go-v2/config v1.18.8 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.13.8 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.10.9 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression v1.4.35 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.21 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.27 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.21 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.14.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.21 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.21 // indirect
--- main.go
+++ main.go
@@ -3,32 +3,55 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-	"github.com/aws/aws-sdk-go/aws"
 )
 
 const (
 	AWS_REGION = "us-east-1"
 
-	TABLE_NAME                       = "RequestRecords"
-	EXAMPLE_ITEM_TO_WRITE_ID         = "1234567"
-	EXAMPLE_ITEM_TO_WRITE_COMPANY_ID = "0987654321"
+	TABLE_NAME               = "RequestRecords"
+	ITEM_TO_WRITE_ID         = "1234567"
+	ITEM_TO_WRITE_COMPANY_ID = "0987654321"
 )
 
 func main() {
 	client := setupClient()
 
-	putItem(client, "2023-01-22T09:52:23.616414-08:00")
-	putItem(client, "2023-01-22T09:52:23.616414-08:00")
-	putItem(client, "")
-	putItem(client, "")
+	putItem(client, ITEM_TO_WRITE_ID, "2023-01-22T09:52:23.616414-08:00")
+	putItem(client, ITEM_TO_WRITE_ID, "")
 
-	deleteItem(client, "2023-01-22T09:52:23.616414-08:00")
+	records, _ := queryItems(client, ITEM_TO_WRITE_ID)
+	for i, rec := range records {
+		fmt.Printf("Record #%d: %v\n", i, rec)
+	}
+
+	before := len(records)
+	fmt.Printf("# records BEFORE deletion = %d\n", before)
+
+	deleteItem(client, ITEM_TO_WRITE_ID, "2023-01-22T09:52:23.616414-08:00")
+
+	records, _ = queryItems(client, ITEM_TO_WRITE_ID)
+	for i, rec := range records {
+		fmt.Printf("Record #%d: %v\n", i, rec)
+	}
+
+	after := len(records)
+	fmt.Printf("# records AFTER deletion = %d\n", after)
+
+	if before != (after + 1) {
+		fatalExit("after deletion record count is not correct: expected %d, got %d\n",
+			before-1, after)
+	}
+
 }
 
 func setupClient() *dynamodb.Client {
@@ -50,49 +73,84 @@ determine uniqueness of an entry, setup for the table using the terraform script
 "Id" + "Timestamp" so if these two values are same as those for an existing item in
 the table, then the subsequent item won't be added to the table. */
 
-func putItem(client *dynamodb.Client, timestamp string) {
+func putItem(client *dynamodb.Client, itemId string, timestamp string) {
 
 	if timestamp == "" {
 		currentTime := time.Now()
 		timestamp = currentTime.Format(time.RFC3339Nano)
 	}
 
-	output, err := client.PutItem(context.TODO(), &dynamodb.PutItemInput{
+	_, err := client.PutItem(context.TODO(), &dynamodb.PutItemInput{
 		TableName: aws.String(TABLE_NAME),
 
 		Item: map[string]types.AttributeValue{
-			"Id":        &types.AttributeValueMemberS{Value: EXAMPLE_ITEM_TO_WRITE_ID},
+			"Id":        &types.AttributeValueMemberS{Value: itemId},
 			"Timestamp": &types.AttributeValueMemberS{Value: timestamp},
-			"CompanyId": &types.AttributeValueMemberS{Value: EXAMPLE_ITEM_TO_WRITE_COMPANY_ID},
-			"JSON":      &types.AttributeValueMemberS{Value: "{ \"test\": \"testvalue" + timestamp + "\" }"},
+			"CompanyId": &types.AttributeValueMemberS{Value: ITEM_TO_WRITE_COMPANY_ID},
+			"JSON":      &types.AttributeValueMemberS{Value: "{ \"key\": \"value-" + timestamp + "\" }"},
 		},
 	})
 
 	if err != nil {
 		fatalExit("encountered problem while putting item to table: %v\n", err)
 	}
-
-	fmt.Println(output.Attributes)
 }
 
 /* Attempt to delete an item from the table. The key thing here is to make sure
 the filtering criteria match the schema. */
 
-func deleteItem(client *dynamodb.Client, timestamp string) {
+func deleteItem(client *dynamodb.Client, itemId string, timestamp string) {
 
-	output, err := client.DeleteItem(context.TODO(), &dynamodb.DeleteItemInput{
+	_, err := client.DeleteItem(context.TODO(), &dynamodb.DeleteItemInput{
 		TableName: aws.String(TABLE_NAME),
 
 		Key: map[string]types.AttributeValue{
-			"Id":        &types.AttributeValueMemberS{Value: EXAMPLE_ITEM_TO_WRITE_ID},
+			"Id":        &types.AttributeValueMemberS{Value: itemId},
 			"Timestamp": &types.AttributeValueMemberS{Value: timestamp},
 		},
 	})
 	if err != nil {
 		fatalExit("encountered problem while deleting item from table: %v\n", err)
 	}
+}
 
-	fmt.Println(output.Attributes)
+/* Query and retrieve one or more items with the same "Id" */
+
+type RequestRecord struct {
+	Id        string `dynamodbav:"id"`
+	Timestamp string `dynamodbav:"timestamp"`
+	JSON      string `dynamodbav:"json"`
+}
+
+func queryItems(client *dynamodb.Client, id string) ([]RequestRecord, error) {
+
+	var err error
+	var response *dynamodb.QueryOutput
+
+	var records []RequestRecord
+
+	keyEx := expression.Key("Id").Equal(expression.Value(id))
+	expr, err := expression.NewBuilder().WithKeyCondition(keyEx).Build()
+
+	if err != nil {
+		log.Printf("Couldn't build epxression for query. Here's why: %v\n", err)
+	} else {
+		response, err = client.Query(context.TODO(), &dynamodb.QueryInput{
+			TableName:                 aws.String(TABLE_NAME),
+			ExpressionAttributeNames:  expr.Names(),
+			ExpressionAttributeValues: expr.Values(),
+			KeyConditionExpression:    expr.KeyCondition(),
+		})
+		if err != nil {
+			log.Printf("unable to query for records based on Id: %v: ", err)
+		} else {
+			err = attributevalue.UnmarshalListOfMaps(response.Items, &records)
+			if err != nil {
+				log.Printf("unable to unmarshal query response: %v\n", err)
+			}
+		}
+	}
+	return records, err
 }
 
 func fatalExit(format string, a ...interface{}) {
